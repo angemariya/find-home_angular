b3347e6db7f5435d85896799dbe9d892741260da
Maria Angelova <angelova.m@hotmail.com>
2023-11-14 18:44:18 +0100

    feat: create submitApplication func

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -117,4 +117,8 @@ export class HousingService {
   getHousingLocationById(id: Number): HousingLocation | undefined {
     return this.housingLocationsList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Application submitted for ${firstName} ${lastName} at ${email}`);
+  }
 }
